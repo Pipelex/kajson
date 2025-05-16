9c27900f8473cdcff22e586fb0b4f8e0f7a5a849
Louis Choquel <louis@pipelex.com>
2025-05-16 11:38:42 +0200

    Fix wrongly copy-pasted license header

--- tests/test_serde_union_discrim.py
+++ tests/test_serde_union_discrim.py
@@ -1,6 +1,5 @@
 # SPDX-FileCopyrightText: © 2025 Evotis S.A.S.
-# SPDX-License-Identifier: Elastic-2.0
-# "Pipelex" is a trademark of Evotis S.A.S.
+# SPDX-License-Identifier: Apache-2.0
 
 import logging
 from abc import ABC
